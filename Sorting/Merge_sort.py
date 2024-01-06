8894f4a1fd26d42ca2a01a8e7bd6cdd6d4c25022
Bhuvnesh verma <99537126+MasterBhuvnesh@users.noreply.github.com>
2024-01-06 13:57:20 +0530

    Create Merge_sort.py

---
+++ Sorting/Merge_sort.py
@@ -0,0 +1,30 @@
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+
+def mergesort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = mergesort(arr[:mid])
+    right = mergesort(arr[mid:])
+    return merge(left, right)
+
+
+arr = [8, 7, 2, 1, 0, 9, 6]
+print("Original array:")
+print(arr)
+arr = mergesort(arr)
+print("Sorted array:")
+print(arr)
